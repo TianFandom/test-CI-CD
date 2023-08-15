01ac24f053e18139b171042ef88a12d0f1d55053
tianchen <tchen@fandom.com>
2023-08-15 10:43:52 +0000

    add a file to hold the logs

--- print_test.py
+++ print_test.py
@@ -3,4 +3,9 @@ from datetime import datetime
 # Get the current timestamp
 current_timestamp = datetime.now()
 
+# Print the current timestamp
 print(current_timestamp)
+
+# Append the current timestamp to a .txt file in the same folder
+with open("current_timestamp.txt", "a") as file:
+    file.write(str(current_timestamp) + '\n')
